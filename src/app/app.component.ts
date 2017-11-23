c1fbacff94cc3f2b623e9c84a031644f7895458c
L kabeer <ahmed@Lkabeer.com>
2017-11-23 12:50:21 +0200

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database-deprecated";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  editMsg: boolean = false;
+  editId: number;
+
+  items: FirebaseListObservable<any[]>;
+  msg: string = '';
+
+  constructor(private af: AngularFireDatabase) {
+    this.items = af.list('/messages');
+  }
+
+  send(chatMsg: string) {
+    this.items.push({message: chatMsg});
+    this.msg = '';
+  }
+
+  delete(key: string) {
+    this.items.remove(key);
+  }
+
+  edit(key: string, message: string) {
+    this.items.update(key, {message: message});
+    this.editMsg = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database-deprecated";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http/src/http_module';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAEYJPSnXKBSTsqYUTpXyi9seSP_RrbnEo",
+  authDomain: "creatingcrudapp.firebaseapp.com",
+  databaseURL: "https://creatingcrudapp.firebaseio.com",
+  projectId: "creatingcrudapp",
+  storageBucket: "creatingcrudapp.appspot.com",
+  messagingSenderId: "30783503389"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +24,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
